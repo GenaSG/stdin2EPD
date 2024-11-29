0c1a0cf1e45a1fc3290d3b4ff2d01f33c90d2251
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2024-11-29 10:00:14 +0100

    adding dependencies bundling

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ import sys
 import os
 libdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(libdir)
+sys.path.append(os.path.join(libdir,'lib'))
 
 from main import main
 
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pip3 install --upgrade --target ./lib/ gpiozero spidev RPi.GPIO
 zip -x ./env/**\* -x ./**/__pycache__/**\* -x build.sh -r ../stdin2EPD.zip *
 echo '#!/usr/bin/env python3' | cat - ../stdin2EPD.zip > ../stdin2EPD.pyz
 chmod +x ../stdin2EPD.pyz 
