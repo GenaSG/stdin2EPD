bd647830c3c06c5b4e001ac9d14364805e656e2a
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2024-11-29 09:17:31 +0100

    adding args

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import os
 
 import logging
+import argparse
 from waveshare_epd import epd7in5_V2
 from PIL import Image,ImageDraw,ImageFont
 #import traceback
@@ -11,6 +12,11 @@ from PIL import Image,ImageDraw,ImageFont
 #logging.basicConfig(level=logging.DEBUG)
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--font_size', dest='font_size', default=24, type=int, help='Set font size. Default 24')
+    #parser.add_argument('--partial', dest='partial', type=str, help='insert text with as region StartX,StartY,EndX,Endy')
+    args = parser.parse_args()
+    #print(args.partial)
     text_to_print = ''
     for line in sys.stdin:
         text_to_print += line
@@ -18,13 +24,20 @@ def main():
     epd.init()
 #    epd.Clear()
 
-    font = ImageFont.truetype('DejaVuSansMono.ttf', 24)
+    font = ImageFont.truetype('DejaVuSansMono.ttf', args.font_size)
 
-    epd.init_fast()
     Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
-    draw.text((10, 0), text_to_print, font = font, fill = 0)
-    epd.display(epd.getbuffer(Himage))
+    #draw.text((10, 0), text_to_print, font = font, fill = 0)
+    if hasattr(args, 'partial') and args.partial != None:
+        epd.init_fast()
+        offsets = [int(n) for n in args.partial.split(",")]
+        draw.rectangle((offsets[0],offsets[1],offsets[2],offsets[3]), fill=255)
+        draw.text((offsets[0],offsets[1]), text_to_print, font = font, fill = 0)
+        epd.display_Partial(epd.getbuffer(Himage),offsets[0],offsets[1],offsets[2],offsets[3])
+    else:
+        draw.text((10, 0), text_to_print, font = font, fill = 0)
+        epd.display(epd.getbuffer(Himage))
     #epd.display_Partial(epd.getbuffer(Himage),0, 0, epd.width, epd.height)
     return
 
